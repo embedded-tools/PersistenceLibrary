f8bcf8a4eb6eb7d07c8c54567f9917b73cc9b683
Ondrej Sterba <osterba@inbox.com>
2016-11-28 00:52:18 +0100

    TFilePath supports both separators / and \

--- Source/BasicTypes/TFilePath.cpp
+++ Source/BasicTypes/TFilePath.cpp
@@ -19,13 +19,26 @@
 TFilePath::TFilePath()
 :TCustomString<FILEPATH_MAXLENGTH>()
 {
-
+	m_separator = '\\';
 }
 
 TFilePath::TFilePath(const char* text, unsigned short textLength)
 :TCustomString<FILEPATH_MAXLENGTH>(text, textLength)
 {
+	m_separator = '\\';
+	if (PData!=NULL)
+	{
+		for(int i = 0; i<DataLen; i++)
+		{
+			if (PData[i]=='\\') m_separator='\\';
+			if (PData[i]=='/') m_separator='/';
+		}
+	}
+}
 
+void TFilePath::ChangeSeparator(char separator)
+{
+	m_separator = separator;
 }
 
 const char* TFilePath::ExtractFileName(const char* filename)
@@ -82,7 +95,7 @@ TFilePath TFilePath::ExtractRelativePath (const char* dir, const char* basedir)
 	{
 		//case insensitive string comparison
 		if ((*ss1 & 0xDF) != (*ss2 & 0xDF)) break;
-		if (*ss1=='\\') 
+		if ((*ss1=='\\') || (*ss1=='/'))
         {
 			pp1=ss1;  pp2=ss2; 
 			pp1++;    pp2++;
@@ -97,12 +110,17 @@ TFilePath TFilePath::ExtractRelativePath (const char* dir, const char* basedir)
 	{
 		while (*pp2!=0) 
 		{
-			if ((*pp2=='\\') || (*pp2=='/')) result += "..\\";
+			if ((*pp2=='\\') || (*pp2=='/')) 
+			{
+				result += "..";
+				result += m_separator;
+			}
 			pp2++;
 		}
 		if ((basedir[StrLen(basedir)-1]!='\\') && (basedir[StrLen(basedir)-1]!='/')) 
 		{
-			result += "..\\";
+			result += "..";
+			result += m_separator;
 		}
 		if (pp1==dir) result=(const char*)pp1;
 		else result += (const char*)pp1;
@@ -120,7 +138,7 @@ bool TFilePath::ChangeFileExt (const char* ext)
 			pos = i;
 			break;
 		}
-		if (PData[i]=='\\')
+		if ((PData[i]=='\\') || (PData[i]=='/'))
 		{
 			break;
 		}
@@ -144,7 +162,7 @@ bool TFilePath::ChangeFileName (const char* filename)
 	int pos = Length();
 	for(int i = Length()-1; i>=0; i--)
 	{
-		if (PData[i]=='\\')
+		if ((PData[i]=='\\') || (PData[i]=='/'))
 		{
 			pos = i + 1;
 			break;
@@ -164,4 +182,221 @@ bool TFilePath::ChangeFileName (const char* filename)
 	return true;
 };
 
+void TFilePath::DeleteFileName()
+{
+	int i = LastIndexOf('\\');
+	if (i==-1)
+	{
+		i = LastIndexOf('/');
+	}
+	if (i!=-1)
+	{
+		SetLength(i+1);
+	}
+}
 
+void TFilePath::DeleteLastDir()
+{
+    unsigned short i;
+	
+	if (Length()==3)
+	{
+		if ( ((*this)[0]=='.')  && 
+			 ((*this)[1]=='.')  && 
+			 ( ((*this)[2]=='\\') || ((*this)[2]=='/') )
+		   )   
+		{
+			SetLength(2);
+			(*this)[1]=m_separator;
+			return;
+		}
+	}
+	bool slashFound = false;
+    for (i = Length()-2; i>0; i--) 
+    {
+        if ( ((*this)[i]=='\\') || ((*this)[i]=='/') )
+        {
+            SetLength(i+1);
+			slashFound = true;
+            break;
+        }
+		if ((*this)[i]==':')
+		{
+			slashFound = true;
+			break;
+		}
+    }
+	if (!slashFound)
+	{
+		if (Length()==2)
+		{
+			if ((PData[0]=='.') && ( (PData[1]=='\\') || (PData[1]=='/')) )
+			{
+				return;
+			}
+		}
+		Clear();
+	}
+}
+
+
+TFilePath TFilePath::operator = ( TFilePath& oString )
+{
+	CopyFrom(oString.ToPChar());
+	for(int i = 0; i<DataLen; i++)
+	{
+		if (i=='\\') m_separator='\\';
+		if (i=='/') m_separator='/';
+	}
+	return *this;    
+}
+
+TFilePath& TFilePath::operator = ( TString& oString )
+{
+	CopyFrom(oString.ToPChar());
+	for(int i = 0; i<DataLen; i++)
+	{
+		if (i=='\\') m_separator='\\';
+		if (i=='/') m_separator='/';
+	}
+	return *this;
+}
+
+TFilePath& TFilePath::operator = ( const char* pChar)
+{
+	CopyFrom(pChar);
+	return *this;
+}
+
+TFilePath TFilePath::operator + ( const char* pChar )
+{
+	TFilePath result = *this;
+	result+=pChar;
+	return result;
+}
+
+TFilePath& TFilePath::operator += ( const char* pChar )
+{
+	if (pChar==NULL)
+	{
+		return *this;
+	}
+
+    int pCharLen = 0;
+
+	if (LastChar()>=' ')
+	{
+		if ((LastChar()!='\\') && (LastChar()!='/'))
+		{
+			SetLength(DataLen+1,false);
+			if (PData!=NULL)
+			{
+				PData[DataLen-1] = m_separator;						
+			}
+		}
+	}
+	if (pChar!=0)
+	{
+        while (pChar[0]=='.')
+        {
+			DeleteFileName();
+			pChar++;
+            if ((pChar[0]=='.'))
+            {
+                pChar++;
+				if ((pChar[0]=='\\') || (pChar[0]=='/'))
+				{
+					pChar++;
+				}
+                DeleteLastDir();
+            } else
+			if ((pChar[0]=='\\') || (pChar[0]=='/'))
+            {
+                pChar++;
+            } else break;
+        }
+    };			
+	if ((LastChar()=='\\') || (LastChar()=='/'))
+	{
+		if ((*pChar=='\\') || (*pChar=='/'))
+		{
+			pChar++;
+		}
+	}
+	if (pChar!=NULL)
+	{
+		pCharLen = 0;
+		if (pChar!=NULL)
+		{
+			pCharLen = strlen(pChar);    
+		}
+	}
+	if (pCharLen==0) return *this;
+	unsigned short oldLength = Length();
+	unsigned short newLength = oldLength + pCharLen;
+	if (newLength<oldLength) newLength=65534;
+	if (newLength==65535) newLength=65534;
+	newLength = SetLength(newLength);        
+	if (newLength>oldLength)
+	{
+		memcpy(&PData[oldLength], pChar, newLength-oldLength);
+		if (PData)
+		{
+			for(int i = 0; i<DataLen; i++)
+			{
+				if ((PData[i]=='\\') || (PData[i]=='/'))
+				{
+					if (PData[i]!=m_separator)
+					{
+						PData[i]=m_separator;
+					}
+				}
+			}	
+		}
+	}
+	return *this;
+}
+
+TFilePath&	TFilePath::operator += ( const char c)
+{
+	int oldLength = DataLen;
+	SetLength(DataLen+1, false);
+	if (DataLen>oldLength)
+	{
+		PData[DataLen-1] = c;
+	}
+	return *this;
+}
+
+bool TFilePath::operator == (TCustomString<FILEPATH_MAXLENGTH>& s)
+{
+	if ((ToPChar()==NULL) && (s.ToPChar()==NULL)) return true;
+	if (ToPChar()==NULL) return false;
+	if (s.ToPChar()==NULL) return false;
+	return strcmp(ToPChar(), s.ToPChar()) == 0;
+}
+
+bool TFilePath::operator == (const char* pChar)
+{
+	if ((ToPChar()==NULL) && (pChar==NULL)) return true;
+	if ((ToPChar()==NULL) && (*pChar==0)) return true;
+	if (ToPChar()==NULL) return false;
+	if (pChar==NULL) return false;
+	return strcmp(ToPChar(), pChar) == 0;
+}
+
+bool TFilePath::operator != (TCustomString<FILEPATH_MAXLENGTH>& s)
+{
+	if ((ToPChar()==NULL) && (s.ToPChar()==NULL)) return false;
+	if (ToPChar()==NULL) return true;
+	if (s.ToPChar()==NULL) return true;
+	return strcmp(ToPChar(), s.ToPChar()) != 0;
+}
+
+bool TFilePath::operator != (const char* pChar)
+{
+	if ((ToPChar()==NULL) && (pChar==NULL)) return false;
+	if (ToPChar()==NULL) return true;
+	if (pChar==NULL) return true;
+	return strcmp(ToPChar(), pChar) != 0;
+}
--- Source/BasicTypes/TFilePath.h
+++ Source/BasicTypes/TFilePath.h
@@ -25,9 +25,13 @@
 
 class TFilePath : public TCustomString<FILEPATH_MAXLENGTH>
 {
+	private:
+		char m_separator;
 	public:
 		TFilePath();
 		TFilePath(const char* text, unsigned short textLength=0);
+
+		void ChangeSeparator(char separator);
 
 		const char* ExtractFileName(const char* filename);
 		const char* ExtractFileExt(const char* filename);
@@ -38,182 +42,18 @@ class TFilePath : public TCustomString<FILEPATH_MAXLENGTH>
 		bool        ChangeFileExt  (const char* pszNewExtension);
 		bool        ChangeFileName (const char* pszNewFilename);
 
-
-		TFilePath operator = ( TFilePath& oString )
-		{
-			CopyFrom(oString.ToPChar());
-			return *this;    
-		}
-
-		TFilePath& operator = ( TString& oString )
-		{
-			CopyFrom(oString.ToPChar());
-			return *this;
-		}
-
-		TFilePath& operator = ( const char* pChar)
-		{
-			CopyFrom(pChar);
-			return *this;
-		}
-
-		void DeleteFileName()
-		{
-			int i = LastIndexOf('\\');			
-			SetLength(i+1);
-		}
-        
-        void DeleteLastDir()
-        {
-            unsigned short i;
-			
-			if (Length()==3)
-			{
-				if (((*this)[0]=='.') && ((*this)[1]=='.') && ((*this)[2]=='\\'))
-				{
-					SetLength(2);
-					(*this)[1]='\\';
-					return;
-				}
-			}
-			bool slashFound = false;
-            for (i = Length()-2; i>0; i--) 
-            {
-                if ((*this)[i]=='\\')
-                {
-                    SetLength(i+1);
-					slashFound = true;
-                    break;
-                }
-				if ((*this)[i]==':')
-				{
-					slashFound = true;
-					break;
-				}
-            }
-			if (!slashFound)
-			{
-				if (Length()==2)
-				{
-					if ((PData[0]=='.') && (PData[1]=='\\'))
-					{
-						return;
-					}
-				}
-				Clear();
-			}
-        }
-
-		TFilePath operator + ( const char* pChar )
-		{
-			TFilePath result = *this;
-			result+=pChar;
-			return result;
-		}
-
-		TFilePath& operator += ( const char* pChar )
-		{
-			if (pChar==NULL)
-			{
-				return *this;
-			}
-
-            int pCharLen = 0;
-
-			if (LastChar()>=' ')
-			{
-				if (LastChar()!='\\')
-				{
-					SetLength(DataLen+1,false);
-					if (PData!=NULL)
-					{
-						PData[DataLen-1] = '\\';						
-					}
-				}
-			}
-			if (pChar!=0)
-			{
-                while (pChar[0]=='.')
-                {
-					DeleteFileName();
-					pChar++;
-                    if ((pChar[0]=='.'))
-                    {
-                        pChar++;
-						if (pChar[0]=='\\')
-						{
-							pChar++;
-						}
-                        DeleteLastDir();
-                    } else
-					if (pChar[0]=='\\')
-                    {
-                        pChar++;
-                    } else break;
-                }
-            };			
-			if (LastChar()=='\\')
-			{
-				if (*pChar=='\\')
-				{
-					pChar++;
-				}
-			}
-			if (pChar!=NULL)
-			{
-				pCharLen = 0;
-				if (pChar!=NULL)
-				{
-					pCharLen = strlen(pChar);    
-				}
-			}
-			if (pCharLen==0) return *this;
-			unsigned short oldLength = Length();
-			unsigned short newLength = oldLength + pCharLen;
-			if (newLength<oldLength) newLength=65534;
-			if (newLength==65535) newLength=65534;
-			newLength = SetLength(newLength);        
-			if (newLength>oldLength)
-			{
-				memcpy(&PData[oldLength], pChar, newLength-oldLength);
-			}
-			return *this;
-		}
-
-
-
-		bool operator == (TCustomString<FILEPATH_MAXLENGTH>& s)
-		{
-			if ((ToPChar()==NULL) && (s.ToPChar()==NULL)) return true;
-			if (ToPChar()==NULL) return false;
-			if (s.ToPChar()==NULL) return false;
-			return strcmp(ToPChar(), s.ToPChar()) == 0;
-		}
-
-		bool operator == (const char* pChar)
-		{
-			if ((ToPChar()==NULL) && (pChar==NULL)) return true;
-			if ((ToPChar()==NULL) && (*pChar==0)) return true;
-			if (ToPChar()==NULL) return false;
-			if (pChar==NULL) return false;
-			return strcmp(ToPChar(), pChar) == 0;
-		}
-
-		bool operator != (TCustomString<FILEPATH_MAXLENGTH>& s)
-		{
-			if ((ToPChar()==NULL) && (s.ToPChar()==NULL)) return false;
-			if (ToPChar()==NULL) return true;
-			if (s.ToPChar()==NULL) return true;
-			return strcmp(ToPChar(), s.ToPChar()) != 0;
-		}
-
-		bool operator != (const char* pChar)
-		{
-			if ((ToPChar()==NULL) && (pChar==NULL)) return false;
-			if (ToPChar()==NULL) return true;
-			if (pChar==NULL) return true;
-			return strcmp(ToPChar(), pChar) != 0;
-		}
+		void		DeleteFileName();
+		void		DeleteLastDir();
+		TFilePath	operator = ( TFilePath& oString );
+		TFilePath&	operator = ( TString& oString );
+		TFilePath&	operator = ( const char* pChar);
+		TFilePath	operator + ( const char* pChar );
+		TFilePath&	operator += ( const char* pChar );
+		TFilePath&	operator += ( const char c);
+		bool		operator == (TCustomString<FILEPATH_MAXLENGTH>& s);
+		bool		operator == (const char* pChar);
+		bool		operator != (TCustomString<FILEPATH_MAXLENGTH>& s);
+		bool		operator != (const char* pChar);
 
 };
 
