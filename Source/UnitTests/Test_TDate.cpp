bd17418b7b5bc4cb3e65c6d8ebfb00010be27a74
Ondrej Sterba <osterba@inbox.com>
2017-10-14 05:17:58 +0200

    TDate - new tests added

--- Source/UnitTests/Test_TDate.cpp
+++ Source/UnitTests/Test_TDate.cpp
@@ -14,6 +14,10 @@ class Test_TDate : public TestFixture<Test_TDate>
         TEST_CASE( AddDay );
         TEST_CASE( AddMonth );
         TEST_CASE( AddYear );
+		TEST_CASE( GetTotalDays1 );
+		TEST_CASE( GetTotalDays2 );
+		TEST_CASE( LeapYearTest );
+		TEST_CASE( DateDifference );
     }
 
 
@@ -121,11 +125,110 @@ class Test_TDate : public TestFixture<Test_TDate>
         ASSERT(date3.GetYear()==2015);
         ASSERT(date3.GetMonth()==3);
         ASSERT(date3.GetDay()==1);
-
-
     }
 
+	void GetTotalDays1()
+	{
+		TDate date1(1970, 1,  1);
+		TDate date2(1970, 2,  1);
+		TDate date3(1970, 3,  1);
+		TDate date4(1970, 4,  1);
+		TDate date5(1971, 1,  1);
+		int days;
 
+		days = date1.GetTotalDays();
+		ASSERT_EQUALS(0, days);
+
+		days = date2.GetTotalDays();
+		ASSERT_EQUALS(31, days);
+
+		days = date3.GetTotalDays();
+		ASSERT_EQUALS(59, days);
+
+		days = date4.GetTotalDays();
+		ASSERT_EQUALS(90, days);
+
+		days = date5.GetTotalDays();
+		ASSERT_EQUALS(365, days);
+	}
+
+	void GetTotalDays2()
+	{
+		TDate date1(1972, 1,   1);
+		TDate date2(1972, 2,   1);
+		TDate date3(1972, 3,   1);
+		TDate date4(1972, 4,   1);
+		TDate date5(1972, 12, 31);
+		TDate date6(1973, 1,   1);
+		TDate date7(2149, 6,   5);
+		int days;
+
+		days = date1.GetTotalDays();
+		ASSERT_EQUALS(730, days);
+
+		days = date2.GetTotalDays();
+		ASSERT_EQUALS(761, days);
+
+		days = date3.GetTotalDays();
+		ASSERT_EQUALS(790, days);
+
+		days = date4.GetTotalDays();
+		ASSERT_EQUALS(821, days);
+
+		days = date5.GetTotalDays();
+		ASSERT_EQUALS(1095, days);
+
+		days = date6.GetTotalDays();
+ 		ASSERT_EQUALS(1096, days);
+
+		days = date7.GetTotalDays();
+		ASSERT_EQUALS(1096, days);
+
+	}
+
+	void LeapYearTest()
+	{
+		TDate date1(1999, 1,  1);
+		TDate date2(2000, 1,  1);
+		TDate date3(2001, 1,  1);
+		TDate date4(2002, 1,  1);
+		TDate date5(2003, 1,  1);
+		TDate date6(2004, 1,  1);
+
+		int difference;
+
+		difference = date2.GetTotalDays() - date1.GetTotalDays();
+		ASSERT_EQUALS(365, difference);
+
+		difference = date3.GetTotalDays() - date2.GetTotalDays();
+		ASSERT_EQUALS(366, difference);
+
+		difference = date4.GetTotalDays() - date3.GetTotalDays();
+		ASSERT_EQUALS(365, difference);
+
+		difference = date5.GetTotalDays() - date4.GetTotalDays();
+		ASSERT_EQUALS(365, difference);
+
+		difference = date6.GetTotalDays() - date5.GetTotalDays();
+		ASSERT_EQUALS(365, difference);
+	}
+
+	void DateDifference()
+	{
+		TDate date1(1999, 12,  31);
+		TDate date2(2000, 1,  1);
+
+		TDate date3(2001, 7,  31);
+		TDate date4(2001, 8,  1);
+
+		int difference;
+
+		difference = date2.GetTotalDays() - date1.GetTotalDays();
+		ASSERT_EQUALS(1, difference);
+
+		difference = date4.GetTotalDays() - date3.GetTotalDays();
+		ASSERT_EQUALS(1, difference);
+	}
 
 
 };
